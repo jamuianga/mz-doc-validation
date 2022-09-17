b4524a4c08435f27ff9885733dba4cefc428b306
Jânio Muianga <janiodrey@gmail.com>
2022-09-17 13:42:36 +0200

    validação de BI, Carta de condução e NUIT

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,97 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
 import './App.scss';
 
 function App() {
-  const [msg, setMsg] = useState('');
-  const [tipoDoc, setTipoDoc] = useState(0);
-  const [doc, setDoc] = useState('');
+  const [documento, setDocumento] = useState(1);
+  const [numeroDocumento, setNumeroDocumento] = useState('');
+  const [showValidacao, setShowValidacao] = useState(false);
+  const [isDocumentoValido, setIsDocumentoValido] = useState(false);
 
-  const validarDoc = () => {
+  const validarDocumento = () => {
     let regex = null;
-    console.log(tipoDoc);
 
-    switch (tipoDoc) {
-      case '1':
-        regex = /[0-9]{12}[A-Z]{1}/;
+    switch (documento) {
+      case 1:
+        regex = /^[0-9]{12}[A-Z]{1}$/;
         break;
-      case '2':
+      case 2:
         regex = /[0-9]{8}\/[0-9]{1}/;
+        break;
+      case '4':
+        regex = /^[0-9]{9}$/;
         break;
       default:
         alert('Indique o tipo de documento que pretende validar');
         return;
     }
 
-    if (doc.match(regex) != null) {
-      setMsg('Válido!');
+    if (numeroDocumento.match(regex) != null) {
+      setIsDocumentoValido(true);
     } else {
-      setMsg('Inválido!');
+      setIsDocumentoValido(false);
     }
+
+    setShowValidacao(true);
+  };
+
+  const resetForm = () => {
+    setDocumento(1);
+    setNumeroDocumento('');
+    setShowValidacao(false);
   };
 
   return (
-    <div className="App">
-      <div className="form-group">
-        <select
-          onChange={(e) => {
-            setTipoDoc(e.target.value);
-          }}
+    <>
+      <h1>Validação de documentos</h1>
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className={`${showValidacao ? 'form-validation' : ''}`}
+      >
+        <div className="form-input-group">
+          <label>Documento</label>
+          <select
+            className="form-input"
+            value={documento}
+            onChange={(e) => {
+              setDocumento(e.target.value);
+            }}
+          >
+            <option value={1}>Bilhete de identidade</option>
+            <option value={2}>Carta de condução</option>
+            <option value={3}>Passaporte</option>
+            <option value={4}>NUIT</option>
+          </select>
+        </div>
+
+        <div
+          className={`form-input-group ${
+            isDocumentoValido ? 'valid' : 'invalid'
+          }`}
         >
-          <option value={0}>Tipo de documento</option>
-          <option value={1}>Bilhete de identidade</option>
-          <option value={2}>Carta de condução</option>
-          <option value={3}>Passaporte</option>
-          <option value={4}>NUIT</option>
-        </select>
-        <input
-          type="text"
-          value={doc}
-          onChange={(e) => setDoc(e.target.value)}
-        />
-        <button onClick={validarDoc}>Validar</button>
-        <small>{msg}</small>
-      </div>
-    </div>
+          <label htmlFor="">Número do documento</label>
+          <div className="input-group">
+            <input
+              className="form-input"
+              type="text"
+              value={numeroDocumento}
+              onChange={(e) => setNumeroDocumento(e.target.value)}
+            />
+            {showValidacao && (
+              <span className={'material-icons'}>
+                {isDocumentoValido ? 'done' : 'close'}
+              </span>
+            )}
+          </div>
+        </div>
+
+        <button className="form-btn" type="button" onClick={validarDocumento}>
+          Validar
+        </button>
+        <button className="form-btn" type="button" onClick={resetForm}>
+          Repor
+        </button>
+      </form>
+    </>
   );
 }
 
