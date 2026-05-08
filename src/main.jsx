371c62a2677c8cb58cad9a51cd8faffc9e1b2976
Janio Muianga <janiodrey@gmail.com>
2026-05-08 15:46:20 +0200

    update vite and react

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import 'material-icons/iconfont/material-icons.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./main.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+import App from "./App";
+
+import "material-icons/iconfont/material-icons.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
